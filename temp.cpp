27b697239617c5a29b5556961e93206dccba881c
kenn kanja <kennedy.karanja2@gmail.com>
2023-03-03 07:11:02 -0800

    second commit

--- temp.cpp
+++ temp.cpp
@@ -1,4 +1,15 @@
 //This  is a new file called tem.cpp
-//C-x C-f :Opens a file, asks for file name
+//C-x C-f :Opens a file, asks for file name. If it cannot find the file, creates the file
+//C-x C-s :Saves the file without a prompt
+//C-x s : Saves  all files with a prompt
+//C-s C-w: Saves  the file with a different name.Ask you for the name.
+
+
+//
+//Everytime you save a file, Emacs creates another  file with the name "filename~".
+//This tilde(~) fil is the previous version of the file. It will be in the same dir.
+//Also, Emacs auto-saves everything you type to a file with the name "#filename#"
+//If you quit Emacs withour saving, you can see this auto-save file.let's do that.
+//C-x C-c:Quits Emacs
 
 
